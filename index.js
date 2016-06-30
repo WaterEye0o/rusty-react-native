29c1c2d34b141eb8e479850fb6a8aa71a6f51c19
mingmourushui <13804087628@163.com>
2016-06-30 17:20:05 +0800

    release BrowseBottom Navigator styleSheet DottedLine LoadingModal

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict'
 
 import OverflowView from './lib/OverflowView'
-export {OverflowView}
+import BrowseBottom from './lib/BrowseBottom'
+import Navigator from './lib/CustomNavigator'
+var StyleSheet = require( './lib/CustomStyleSheet')
+import DottedLine from './lib/DottedLine'
+import LoadingModal from './lib/LoadingModal'
+
+export {
+  OverflowView,
+  BrowseBottom,
+  Navigator,
+  StyleSheet,
+  DottedLine,
+  LoadingModal
+}
